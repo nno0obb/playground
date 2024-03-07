4f89d07935a8e5bb8a7ff2ca4dbbbd657222fc7a
bt.7274(김홍균)/kakao <bt.7274@kakaocorp.com>
2024-03-07 23:36:59 +0900

    ps(leetcode): Update docs

---
+++ ps/leetcode/NO_slow.py
@@ -0,0 +1,12 @@
+import os
+from typing import *
+
+
+class Solution:
+    def function(self, s: str) -> bool:
+        pass
+
+
+if __name__ == '__main__':
+    os.system('python3 NO_test.py')
+    # os.system('python3 NO_test.py -k testcase_1')
